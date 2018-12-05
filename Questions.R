1bf29b34a9d5fc4163fde9d9d4eaf164c4e23141
Иван Локтаев <ivanloktaev@Da-Joey.local>
2018-12-05 20:03:48 +0300

    #2 and #3 done

--- Questions.R
+++ Questions.R
@@ -2,15 +2,19 @@ setwd(
   "/Users/ivanloktaev/Yandex.Disk.localized/Uni/4\ Курс/Data\ Science/HWs/ha2\ materials/"
 )
 library(haven)
+library(data.table)
 data_1 <- read_dta("data_1.dta")
 # What is done above is initial setup
 
 #1
 # Create two additional dummy variables unem74, unem75
 # Create new variables and then cbind them to the data_1 date.frame
-# Add new variables to the data.frame
+# Add new variables to the data.frame making sure
+# they are written as 1 and 0 no TRUE and FALSE
 data_1 <-
-  cbind(data_1, unem74 = data_1$re74 == 0, unem75 = data_1$re75 == 0)
+  cbind(data_1,
+        unem74 = as.numeric(data_1$re74 == 0),
+        unem75 = as.numeric(data_1$re75 == 0))
 
 # Running univariate regressions, and writing their summaries to respective variables
 reg_age <- summary(lm(data_1$treat ~ data_1$age))
@@ -44,3 +48,31 @@ View(p_matrix)
 # covariates the assumption holds true
 # only nodeg has explanatory power in relation to treat at s.l = 0.05
 # and variables hispanic and unem75 have expl.power at s.l = 0.01
+
+#2
+# Сonverting re78 to thousands
+data_1$re78 <- data_1$re78 / 1000
+# Running regression for #2 and displaying its summary
+summary(lm(data_1$re78 ~ data_1$treat))
+
+# Now look at stimates slope coefficient
+# If for some person job training program was done,
+# earnings in 1978 increase on average by 1.7943 thousand of dollars
+
+#3
+# Running regression for #3 and displaying its summary
+summary(
+  lm(
+    data_1$re78 ~ data_1$treat + data_1$age + data_1$education + data_1$black + data_1$hispanic + data_1$married +
+      data_1$nodegree + data_1$re74 + data_1$re75 + data_1$unem74 + data_1$unem75
+  )
+)
+
+# Only treat coefficient is significant (at alpha = 0.05) in this model
+# The st.error of treat in model 1 was 0.6329,
+# While in the new model it is 6.411e-01, which is sligtly larger.
+# Thus, we do not need to add additional (insignificant) variables
+# and can drop them off the regression
+
+#4
+data_2 <- read_dta("data_2.dta")
