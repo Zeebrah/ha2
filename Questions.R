e5c24bb07419c4f8617917caf5e869fceba19757
Иван Локтаев <ivanloktaev@Da-Joey.local>
2018-12-05 17:24:43 +0300

    Running and saving regression for task 1

--- Questions.R
+++ Questions.R
@@ -10,3 +10,15 @@ data_1 <- read_dta("data_1.dta")
 data_1 <- cbind(data_1,unem74 = data_1$re74 == 0,unem75 = data_1$re75 == 0)
 
 # Running univariate regressions
+reg_age <- lm(data_1$treat ~ data_1$age)
+reg_edu <- lm(data_1$treat ~ data_1$education)
+reg_black <- lm(data_1$treat ~ data_1$black)
+reg_hisp <- lm(data_1$treat ~ data_1$hispanic)
+reg_married <- lm(data_1$treat ~ data_1$married)
+reg_nodeg <- lm(data_1$treat ~ data_1$nodegree)
+reg_re74 <- lm(data_1$treat ~ data_1$re74)
+reg_re75 <- lm(data_1$treat ~ data_1$re75)
+reg_unem74 <- lm(data_1$treat ~ data_1$unem74)
+reg_unem75 <- lm(data_1$treat ~ data_1$unem75)
+
+
