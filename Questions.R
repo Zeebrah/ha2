53f953aeca6cfc71c752912117a1cc0753280d6d
Иван Локтаев <ivanloktaev@Da-Joey.local>
2018-12-09 23:15:34 +0300

    #6b and #7 done

--- Questions.R
+++ Questions.R
@@ -4,6 +4,8 @@ setwd(
 library(haven)
 library(data.table)
 data_1 <- read_dta("data_1.dta")
+# Obviously, seed should be 42
+set.seed(42)
 # What is done above is initial setup
 
 #1
@@ -209,7 +211,7 @@ library(glmnet)
 # from the data set using appropriate column numbers
 # Then cbind these columns to all excluded factors' columns
 x <-
-  as.matrix(cbind(data_non_experimental[, c(-2,-3,-8,-9,-10)], data_non_experimental[, c(2, 3, 8, 9)]))
+  as.matrix(cbind(data_non_experimental[, c(-2, -3, -8, -9, -10)], data_non_experimental[, c(2, 3, 8, 9)]))
 
 # Same thing but more detailed:
 # So we cbind  dummies and non-dummie factors' columns
@@ -221,19 +223,20 @@ x <-
 # It includes all first order interaction b/w covariates
 # So we add 1 to exclude treat variable
 combinations <- combn(10, 2) + 1
-# Predefining the dimensions of the 1st order interaction matrix
-inter <- matrix(nrow = nrow(x), ncol = ncol(combinations))
 
-# Loop to fill the matrix
+# Predefining the dimensions of linear terms interaction matrix
+inter <- matrix(nrow = nrow(x), ncol = ncol(combinations))
+# Creating a set of empty column names for inter
+colnames(inter) <- rep(NA, ncol(combinations))
+# Loop to fill the matrix and set the names
 for (i in 1:ncol(combinations)) {
   inter[, i] <- x[, combinations[1, i]] * x[, combinations[2, i]]
+  # Setting the name of the respective column to represent the contents
+  colnames(inter)[i] <-
+    paste(colnames(x)[combinations[1, i]], colnames(x)[combinations[2, i]], sep =
+            "*")
 }
 
-
-inter<-matrix(nrow=nrow(x),ncol=ncol(combinations))
-for(i in 1:ncol(combinations)){
-  inter[,i]<- x[,combinations[1,i]]*x[,combinations[2,i]]
-}
 # Merge matrices of covariates and their interactions
 first_order_regressors <- cbind(x, inter)
 
@@ -250,21 +253,35 @@ colnames(covariates_sq) <-
 
 #d) All second order ineractions of covariates
 # Here we find all the cross-terms for factors^2 and dummies
+# Again since dummie squared is just dummies
 # Following line defines factors which cross terms we need
 # it contains all dummies except treat and all squares of non-dimmies
-inter_sq_input <- cbind(x[,2:7], covariates_sq)
+inter_sq_input <- cbind(x[, 2:7], covariates_sq)
 
-#To go through all combinations 
-# we use same approach as in b)
-combinations_for_sq <- combn(10, 2)
+#To go through all combinations
+# we can't use same approach as in b)
+# as it would make a ton of excess dummie interactions to appear
+# which are already included
+# combinations_for_sq <- combn(10, 2) - not used
+
+# instead, this specific design of combination matrix would insure
+# that all second order interactions of 2nd order terms are included
+# while not repeating the variables included before
+combinations_d <- rbind(rep(1:6, 4), rep(7:10, 6))
 
 # Predefine the matrix for output
-inter_sq <- matrix(nrow = nrow(x), ncol = ncol(combinations))
+inter_sq <- matrix(nrow = nrow(x), ncol = ncol(combinations_d))
+# Creating a set of empty column names for inter_sq
+colnames(inter_sq) <- rep(NA, ncol(combinations_d))
 
 # Loop to fill the matrix
-for (i in 1:ncol(combinations_for_sq)) {
+for (i in 1:ncol(combinations_d)) {
   inter_sq[, i] <-
-    inter_sq_input[, combinations_for_sq[1, i]] * inter_sq_input[, combinations_for_sq[2, i]]
+    inter_sq_input[, combinations_d[1, i]] * inter_sq_input[, combinations_d[2, i]]
+  # Setting the name of the respective column to represent the contents
+  colnames(inter_sq)[i] <-
+    paste(colnames(inter_sq_input)[combinations_d[1, i]],
+          colnames(inter_sq_input)[combinations_d[2, i]], sep = "*")
 }
 
 # Merge all second order regressors
@@ -284,7 +301,11 @@ colnames(covariates_cube) <-
 # Combine ALL REGRESSORS into a final matrix of all 1st and 2nd order regressors,
 # Adding also third order for non-dummies from e)
 final_regressors <-
-  as.matrix(cbind(first_order_regressors,second_order_regressors,covariates_cube))
+  as.matrix(cbind(
+    first_order_regressors,
+    second_order_regressors,
+    covariates_cube
+  ))
 
 # Fit the lasso model using all computed variables
 cvfit <-
@@ -293,12 +314,11 @@ cvfit <-
 # Obtain coefficients of the model with minimum MSE
 result <- coef(cvfit, s = cvfit$lambda.min)
 result
-# All unnamed (numbered) factors are interactions
-# thus by model chosen through LASSO 
+# thus by model chosen through LASSO
 # the effect of treat on re78 is strictly positive and is interpreted as
-# 1067 dollars of additional earnings in 1978 for people who have received training
+# is an average increase in income (in dollars) from the training
 
-#6 matching estimator
+# 6 Matching estimator
 # Estimate model_baseline
 # a) : i,ii,iii
 library(lmtest)
@@ -308,21 +328,22 @@ data_non_experimental <- as.data.frame(data_non_experimental)
 cut_1 <- matrix(NA, nrow = 6)
 model_baseline <- list('vector')
 model_aux <- list('vector')
-for (i in 2:7){
-model_baseline[[i]] <-
-  glm(treat ~ re74 + re75 + unem74 + unem75 ,
-      data = data_non_experimental,
-      family = "binomial")
-model_aux[[i]] <-
-  glm(
-    data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75 
-    + data_non_experimental$unem74 + data_non_experimental$unem75 + data_non_experimental[,i],
-    family = "binomial")
-cut_1[[i]] <- lrtest(model_baseline[[i]], model_aux[[i]])$Chisq[2]
+for (i in 2:7) {
+  model_baseline[[i]] <-
+    glm(treat ~ re74 + re75 + unem74 + unem75 ,
+        data = data_non_experimental,
+        family = "binomial")
+  model_aux[[i]] <-
+    glm(
+      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75
+      + data_non_experimental$unem74 + data_non_experimental$unem75 + data_non_experimental[, i],
+      family = "binomial"
+    )
+  cut_1[[i]] <- lrtest(model_baseline[[i]], model_aux[[i]])$Chisq[2]
 }
 
 cut_1
-# 1st position is left blank since it corresponds to treat variable 
+# 1st position is left blank since it corresponds to treat variable
 # which os not a covariate
 # So now, we can clearly see that regressor number 4, i.e "black"
 # Has the highest cutoff at 530.1558815, so we updatej model with it and iterate
@@ -331,7 +352,7 @@ cut_1
 cut_2 <- matrix(NA, nrow = 6)
 model_baseline <- list('vector')
 model_aux <- list('vector')
-for (i in c(2,3,5,6,7)){
+for (i in c(2, 3, 5, 6, 7)) {
   # i doesn't go through 4 since black is now in base model
   model_baseline[[i]] <-
     glm(treat ~ re74 + re75 + unem74 + unem75 + black ,
@@ -339,10 +360,11 @@ for (i in c(2,3,5,6,7)){
         family = "binomial")
   model_aux[[i]] <-
     glm(
-      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75 
-      + data_non_experimental$unem74 + data_non_experimental$unem75 + 
-        data_non_experimental$black + data_non_experimental[,i],
-      family = "binomial")
+      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75
+      + data_non_experimental$unem74 + data_non_experimental$unem75 +
+        data_non_experimental$black + data_non_experimental[, i],
+      family = "binomial"
+    )
   cut_2[[i]] <- lrtest(model_baseline[[i]], model_aux[[i]])$Chisq[2]
 }
 cut_2
@@ -355,19 +377,22 @@ cut_2
 cut_3 <- matrix(NA, nrow = 6)
 model_baseline <- list('vector')
 model_aux <- list('vector')
-for (i in c(2,3,5,7)){
+for (i in c(2, 3, 5, 7)) {
   # i doesn't go through 4 since black is now in base model
   model_baseline[[i]] <-
-    glm(treat ~ re74 + re75 + unem74 + unem75 + black + married ,
-        data = data_non_experimental,
-        family = "binomial")
+    glm(
+      treat ~ re74 + re75 + unem74 + unem75 + black + married ,
+      data = data_non_experimental,
+      family = "binomial"
+    )
   model_aux[[i]] <-
     glm(
-      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75 
-      + data_non_experimental$unem74 + data_non_experimental$unem75 + 
+      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75
+      + data_non_experimental$unem74 + data_non_experimental$unem75 +
         data_non_experimental$black + data_non_experimental$married +
-        data_non_experimental[,i],
-      family = "binomial")
+        data_non_experimental[, i],
+      family = "binomial"
+    )
   cut_3[[i]] <- lrtest(model_baseline[[i]], model_aux[[i]])$Chisq[2]
 }
 cut_3
@@ -380,19 +405,22 @@ cut_3
 cut_4 <- matrix(NA, nrow = 6)
 model_baseline <- list('vector')
 model_aux <- list('vector')
-for (i in c(2,3,5)){
+for (i in c(2, 3, 5)) {
   # i doesn't go through 4 since black is now in base model
   model_baseline[[i]] <-
-    glm(treat ~ re74 + re75 + unem74 + unem75 + black + married + nodegree,
-        data = data_non_experimental,
-        family = "binomial")
+    glm(
+      treat ~ re74 + re75 + unem74 + unem75 + black + married + nodegree,
+      data = data_non_experimental,
+      family = "binomial"
+    )
   model_aux[[i]] <-
     glm(
-      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75 
-      + data_non_experimental$unem74 + data_non_experimental$unem75 + 
+      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75
+      + data_non_experimental$unem74 + data_non_experimental$unem75 +
         data_non_experimental$black + data_non_experimental$married +
-        data_non_experimental$nodegree + data_non_experimental[,i],
-      family = "binomial")
+        data_non_experimental$nodegree + data_non_experimental[, i],
+      family = "binomial"
+    )
   cut_4[[i]] <- lrtest(model_baseline[[i]], model_aux[[i]])$Chisq[2]
 }
 cut_4
@@ -406,20 +434,23 @@ cut_4
 cut_5 <- matrix(NA, nrow = 6)
 model_baseline <- list('vector')
 model_aux <- list('vector')
-for (i in c(2,3)){
+for (i in c(2, 3)) {
   # i doesn't go through 4 since black is now in base model
   model_baseline[[i]] <-
-    glm(treat ~ re74 + re75 + unem74 + unem75 + black + married + nodegree + hispanic,
-        data = data_non_experimental,
-        family = "binomial")
+    glm(
+      treat ~ re74 + re75 + unem74 + unem75 + black + married + nodegree + hispanic,
+      data = data_non_experimental,
+      family = "binomial"
+    )
   model_aux[[i]] <-
     glm(
-      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75 
-      + data_non_experimental$unem74 + data_non_experimental$unem75 + 
+      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75
+      + data_non_experimental$unem74 + data_non_experimental$unem75 +
         data_non_experimental$black + data_non_experimental$married +
-        data_non_experimental$nodegree + data_non_experimental$hispanic + 
-        data_non_experimental[,i],
-      family = "binomial")
+        data_non_experimental$nodegree + data_non_experimental$hispanic +
+        data_non_experimental[, i],
+      family = "binomial"
+    )
   cut_5[[i]] <- lrtest(model_baseline[[i]], model_aux[[i]])$Chisq[2]
 }
 cut_5
@@ -432,25 +463,88 @@ cut_5
 cut_6 <- matrix(NA, nrow = 6)
 model_baseline <- list('vector')
 model_aux <- list('vector')
-for (i in c(3)){
+for (i in c(3)) {
   # i doesn't go through 4 since black is now in base model
   model_baseline[[i]] <-
-    glm(treat ~ re74 + re75 + unem74 + unem75 + black + married + nodegree + hispanic + age,
-        data = data_non_experimental,
-        family = "binomial")
+    glm(
+      treat ~ re74 + re75 + unem74 + unem75 + black + married + nodegree + hispanic + age,
+      data = data_non_experimental,
+      family = "binomial"
+    )
   model_aux[[i]] <-
     glm(
-      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75 
-      + data_non_experimental$unem74 + data_non_experimental$unem75 + 
+      data_non_experimental$treat ~ data_non_experimental$re74 + data_non_experimental$re75
+      + data_non_experimental$unem74 + data_non_experimental$unem75 +
         data_non_experimental$black + data_non_experimental$married +
-        data_non_experimental$nodegree + data_non_experimental$hispanic + 
-        data_non_experimental$age  + data_non_experimental[,i],
-      family = "binomial")
+        data_non_experimental$nodegree + data_non_experimental$hispanic +
+        data_non_experimental$age  + data_non_experimental[, i],
+      family = "binomial"
+    )
   cut_6[[i]] <- lrtest(model_baseline[[i]], model_aux[[i]])$Chisq[2]
 }
 cut_6
 
-# FINALLY, case we found a model where all cutoffs are below 1, 
+# FINALLY, case we found a model where all cutoffs are below 1,
 # i.e containing variales to include in the regression for the propensity score
 
-# a) : i,ii,iii
+# b) : i,ii,iii
+# first, let's establish what is the current model_baseline
+# and which interactions ar e candidates for addition
+# First we need to put new interaction covariates into convinient form
+# inter is interactions of linear covatiater and
+# covariates_sq contains all the relevant squares to add to the model
+covariates_6b <- cbind(inter, covariates_sq)
+
+# But we also need treat and base equation to be contained in the data set
+data_6b <- cbind(data_non_experimental, covariates_6b)
+# while loop seem a logical addition on top of existing for loop
+# it would help us choose the best addition to best model in each iteration
+# Let's first save what we had as an optimal equation in 6a)
+base_equation <-
+  as.character("treat ~ re74 + re75 + unem74 + unem75 + black + married + nodegree + hispanic + age")
+
+# Preparation
+model_baseline <- list('vector')
+model_aux <- list('vector')
+# 100 rows just in case
+cut_6b <- matrix(100, nrow = ncol(covariates_6b))
+
+while (max(cut_6b) > 1) {
+  # update the baseline model from the result of the last while loop cicle
+  model_baseline <-
+    glm(as.formula(base_equation),
+        data = data_6b,
+        family = "binomial")
+  
+  # Go as long  as needed to go through all the candidates
+  for (i in 1:ncol(covariates_6b)) {
+    # introducing new regressor from the covariate_6b set by its name
+    add_reg <- colnames(covariates_6b)[i]
+    # Update the formula with new regressor and establish the model_aux
+    model_aux <-
+      glm(as.formula(paste(base_equation, add_reg, sep  = " + ")), data = data_6b,
+          family = "binomial")
+    cut_6b[[i]] <- lrtest(model_baseline, model_aux)$Chisq[2]
+  }
+  # Upadate base model with a regressor corresponding to maximal cutoff
+  update_reg <- colnames(covariates_6b)[which.max(cut_6b)]
+  # introduce break so that we only update the
+  # equation if in current cutoff matrix max value is above 1
+  if (max(cut_6b) < 1) {
+    break
+  }
+  # Update equation for baseline model
+  base_equation <- paste(base_equation, update_reg, sep = " + ")
+}
+base_equation
+
+#7 perdict peropensity score for each observation
+prediction_model <-
+  glm(as.formula(base_equation),
+      data = data_6b,
+      family = "binomial")
+prediction_df <-
+  data.frame(
+    propensity_score = predict(prediction_model, type = "response"),
+    treat = prediction_model$model$treat
+  )
